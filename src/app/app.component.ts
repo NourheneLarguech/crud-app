ed7d763b3fe7258c7482e00df11902e523bcd51d
Nourhene Larguech <nourhene.larguech@ensi-uma.tn>
2023-05-14 19:28:21 +0100

    finish commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,49 @@
-import { Component, OnInit,ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
 import { EmployeeService } from './services/employee.service';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatSort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { CoreService } from './core/core.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
-  displayedColumns: string[] = ['id', 'firstName', 'lastName', 'email','dob','gender','education','company','experience','package','action'];
+  displayedColumns: string[] = ['id', 'firstName', 'lastName', 'email', 'dob', 'gender', 'education', 'company', 'experience', 'package', 'action'];
   dataSource!: MatTableDataSource<any>;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
-  constructor(private _dialog:MatDialog,private _empService:EmployeeService){}
+  constructor(private _dialog: MatDialog, private _empService: EmployeeService,private _coreService:CoreService) { }
   ngOnInit(): void {
-    this.getEmployeeList() ;
+    this.getEmployeeList();
   }
 
-  openAddEditEmpForm(){
-    this._dialog.open(EmpAddEditComponent);    
+  openAddEditEmpForm() {
+    const dialogRef = this._dialog.open(EmpAddEditComponent);
+    dialogRef.afterClosed().subscribe({
+      next: (val) => {
+        if (val) {
+          this.getEmployeeList();
+        }
+      }
+    })
   }
-  getEmployeeList(){
+  getEmployeeList() {
     this._empService.getEmployeeList().subscribe({
-      next: (res)=>{
-         this.dataSource=new MatTableDataSource(res);
-         this.dataSource.sort=this.sort;
-         this.dataSource.paginator=this.paginator;
+      next: (res) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
       },
       error:
         console.log,
-    
-      
+
+
     })
   }
   applyFilter(event: Event) {
@@ -46,13 +54,27 @@ export class AppComponent implements OnInit{
       this.dataSource.paginator.firstPage();
     }
   }
-  deleteEmployee(id:number){
+  deleteEmployee(id: number) {
     this._empService.deleteEmployee(id).subscribe({
-      next: (res)=>{
-        alert('Employee deleted!');
+      next: (res) => {
+       
+        this._coreService.openSnackBar('Employee deleted!','done');
         this.getEmployeeList();
       },
-        error:console.log,
-      });
+      error: console.log,
+    });
+  }
+  openEditForm(data:any) {
+    const dialogRef= this._dialog.open(EmpAddEditComponent,{
+      data,  //data=data === data,
+    });
+    dialogRef.afterClosed().subscribe({
+      next: (val) => {
+        if (val) {
+          this.getEmployeeList();
+        }
+      }
+    })
+  
   }
 }
